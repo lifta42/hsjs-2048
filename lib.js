172b767f06c87e9f2810a2722b0d3fd5fe3f2bca
lifta42 <lifta42@outlook.com>
2017-09-20 17:29:46 +0800

    Some refactor on method's name, etc
    Do not use `new` any more, so there won't be anything related to "OOP", quoted.

--- lib.js
+++ lib.js
@@ -1,14 +1,15 @@
 //
-// Add some comment here.
-// 2017.9.12
+// hsjs-2048/lib.js
+// Add some more comment here.
+// 2017.9.12 by liftA42.
 //
 
 var IO = function(f) {
     return {_action: f};
 };
-// IO.of :: a -> IO a
-IO.of = function(a) {
-    return new IO(function() { return a; });
+// IO.return :: a -> IO a
+IO.return = function(a) {
+    return IO(function() { return a; });
 };
 // IO.bind :: IO a -> (a -> IO b) -> IO b
 IO.bind = function(io, func) {
@@ -16,14 +17,14 @@ IO.bind = function(io, func) {
 };
 // IO.log :: String -> IO String
 IO.log = function(string) {
-    return new IO(function() {
+    return IO(function() {
         console.log(string);
-        return IO.of(string);
+        return IO.return(string);
     });
 };
 // IO.next :: IO a -> IO b -> IO b
 IO.next = function(a, b) {
-    return IO.bind(a, function(_) { return b; });
+    return IO.bind(a, function() { return b; });
 };
 // IO.main :: IO a -> a
 // ONLY ONCE
@@ -34,14 +35,14 @@ IO.main = function(io) {
 var Delay = function(wait, callback) {
     return {_wait: wait, _callback: callback};
 };
-// Delay.of :: Int -> (a -> IO ()) -> Delay a
-Delay.of = function(wait, callback) {
-    return new Delay(wait, callback);
+// Delay.Delay :: Int -> (a -> IO ()) -> Delay a
+Delay.Delay = function(wait, callback) {
+    return Delay(wait, callback);
 };
 // Delay.resolve :: Delay a -> a -> IO ()
 Delay.resolve = function(delay) {
     return function(value) {
-        return new IO(function() {
+        return IO(function() {
             setTimeout(function() {
                 IO.main(delay._callback(value));
             }, delay._wait);
@@ -50,17 +51,17 @@ Delay.resolve = function(delay) {
 };
 // Delay.cons :: Int -> (a -> IO b) -> Delay b -> Delay a
 Delay.cons = function(wait, func, delay) {
-    return Delay.of(wait, function(a) {
-        var iob = IO.bind(IO.of(a), func);
+    return Delay.Delay(wait, function(a) {
+        var iob = IO.bind(IO.return(a), func);
         return IO.bind(iob, Delay.resolve(delay));
     });
 };
 
 // secondLog :: Delay String
-var secondLog = Delay.of(500, IO.log);
+var secondLog = Delay.Delay(500, IO.log);
 // middleMan :: String -> IO String
 var middleMan = function(first) {
-    return IO.next(IO.log(first), IO.of('second'));
+    return IO.next(IO.log(first), IO.return('second'));
 };
 // firstLog :: Delay String
 var firstLog = Delay.cons(500, middleMan, secondLog);
