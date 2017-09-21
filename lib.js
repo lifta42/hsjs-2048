83b248158337d963f2d4cf2eda45448d0d17aed7
lifta42 <lifta42@outlook.com>
2017-09-21 17:36:44 +0800

    Delete `Delay` for now and think about it again

--- lib.js
+++ lib.js
@@ -5,7 +5,7 @@
 //
 
 var IO = function(f) {
-    return {_action: f};
+    return {action: f};
 };
 // IO.return :: a -> IO a
 IO.return = function(a) {
@@ -13,7 +13,7 @@ IO.return = function(a) {
 };
 // IO.bind :: IO a -> (a -> IO b) -> IO b
 IO.bind = function(io, func) {
-    return func(io._action())
+    return func(io.action())
 };
 // IO.log :: String -> IO String
 IO.log = function(string) {
@@ -29,42 +29,5 @@ IO.next = function(a, b) {
 // IO.main :: IO a -> a
 // ONLY ONCE
 IO.main = function(io) {
-    return io._action();
+    return io.action();
 };
-
-var Delay = function(wait, callback) {
-    return {_wait: wait, _callback: callback};
-};
-// Delay.Delay :: Int -> (a -> IO ()) -> Delay a
-Delay.Delay = function(wait, callback) {
-    return Delay(wait, callback);
-};
-// Delay.resolve :: Delay a -> a -> IO ()
-Delay.resolve = function(delay) {
-    return function(value) {
-        return IO(function() {
-            setTimeout(function() {
-                IO.main(delay._callback(value));
-            }, delay._wait);
-        });
-    };
-};
-// Delay.cons :: Int -> (a -> IO b) -> Delay b -> Delay a
-Delay.cons = function(wait, func, delay) {
-    return Delay.Delay(wait, function(a) {
-        var iob = IO.bind(IO.return(a), func);
-        return IO.bind(iob, Delay.resolve(delay));
-    });
-};
-
-// secondLog :: Delay String
-var secondLog = Delay.Delay(500, IO.log);
-// middleMan :: String -> IO String
-var middleMan = function(first) {
-    return IO.next(IO.log(first), IO.return('second'));
-};
-// firstLog :: Delay String
-var firstLog = Delay.cons(500, middleMan, secondLog);
-// firstIO :: IO ()
-var firstIO = Delay.resolve(firstLog)('first');
-IO.main(firstIO);
