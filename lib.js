46b606194c36d6283b78e82ea8159e31aa8e6d05
lifta42 <lifta42@outlook.com>
2017-09-30 19:44:52 +0800

    Delete many things

--- lib.js
+++ lib.js
@@ -1,138 +1,17 @@
 //
 // hsjs-2048/lib.js
 // Add some more comment here.
-// 2017.9.12 by liftA42.
+// Created: 2017.9.12 by liftA42.
+// Restart: 2017.9.27 by liftA42.
 //
 var HSJS_2048_LIB_JS = true;
 
 
-var IO = function(f) {
-    return {action: f};
-};
-// IO.return :: a -> IO a
-IO.return = function(a) {
-    return IO(function() { return a; });
-};
-// IO.bind :: IO a -> (a -> IO b) -> IO b
-IO.bind = function(io, func) {
-    return func(io.action())
-};
-// IO.log :: String -> IO ()
-IO.log = function(string) {
-    return IO(function() {
-        console.log(string);
-        return IO.return(null);
-    });
-};
-// IO.next :: IO a -> IO b -> IO b
-IO.next = function(a, b) {
-    return IO.bind(a, function() { return b; });
-};
-// IO.main :: IO a -> a
-// ONLY ONCE
-IO.main = function(io) {
-    return io.action();
-};
-
-var Delay = function(f) {
-    // Delay a b
-    // f :: a -> (b -> c) -> ()
-    return {delayed: f};
-};
-// Delay.Timeout :: Int -> (a -> IO b) -> Delay a b
-Delay.Timeout = function(wait, delayed) {
-    return Delay(function(value) {
-        return function(follower) {
-            setTimeout(function() {
-                var ioB = delayed(value);
-                IO.bind(ioB, follower);
-            }, wait);
-            return null;
-        };
-    })
-};
-
-var DelayList = function(f) {
-    // DelayList a
-    // f :: a -> ()
-    return {delayed: f};
-};
-// DelayList.Empty :: DelayList a
-DelayList.Empty = function() {
-    return DelayList(function() {
-        return null;
-    });
-};
-// DelayList.Cons :: Delay a b -> DelayList b -> DelayList a
-DelayList.Cons = function(delay, list) {
-    // return DelayList(function(a) {
-    //     delay.delayed(a, list.delayed);  // list.delay will be called with b
-    //     return null;
-    // });
-    return DelayList(compose(function() { return null; }, flip(delay.delayed)(list.delayed)));
-};
-// DelayList.resolve :: a -> DelayList a -> IO ()
-DelayList.resolve = function(init, list) {
-    return IO(function() {
-        list.delayed(init);
-    });
-};
-
-var Listen = function(f) {
-    return {action: f};
-};
-// Listen.DocEvent :: String -> Listen
-Listen.DocEvent = function(eventName) {
-    return Listen(function(callback) {
-        // Listen once by default, because no remove event listener API is provided by now.
-        // noinspection JSCheckFunctionSignatures
-        document.addEventListener(eventName, callback, {once: true});
-        return null;
-    });
-};
-// Listen.listen :: Listen -> (_Event -> IO ()) -> IO ()
-Listen.listen = function(event, callback) {
-    return IO(function() {
-        event.action(callback);
-        return null;
-    });
-};
-
-// Delay.Listened :: Listen -> (_Event -> a -> IO b) -> Delay a b
-Delay.Listened = function(listen, delayed) {
-    return Delay(function(value) {
-        return function(follower) {
-            return IO.main(Listen.listen(listen, function(ev) {
-                var ioB = delayed(ev, value);
-                IO.bind(ioB, follower);
-                return null;
-            }));
-        };
-    });
-};
-
-// Something for point-free.
-// prop :: String -> _Object -> a
-var prop = function(key) {
-    return function(obj) {
-        return obj[key];
+var willLog = function(log) {
+    return function() {
+        console.log(log);
     };
 };
-// compose :: (b -> c) -> (a -> b) -> a -> c
-var compose = function(g, f) {
-    return function(x) {
-        return g(f(x));
-    };
-};
-// flip :: (a -> b -> c) -> b -> a -> c
-var flip = function(f) {
-    return function(b) {
-        return function(a) {
-            return f(a)(b);
-        };
-    };
-};
-// constant :: a -> b -> a
-var constant = function(a) {
-    return a;
+var willSeq = function(will1, will2) {
+    return will2(will1());
 };
--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 // hsjs-2048/test.js
 // 2017.9.21 by liftA42.
 //
-if (typeof HSJS_2048_LIB_JS === 'undefined') {
+if (typeof window.HSJS_2048_LIB_JS === 'undefined') {
     throw Error('Library (lib.js) is not imported.');
 }
 
@@ -60,10 +60,9 @@ var Test = function() {
 };
 
 
-// safeCheckAndDo :: IO () -> ()
 var safeCheckAndDo = function(action) {
     var test = new Test();
-    test.after = function() { return IO.main(action); };
+    test.after = action;
     var it = test.it;
 
     it('should pass', function(assert) {
@@ -80,114 +79,14 @@ var safeCheckAndDo = function(action) {
         assert(firstFinished);
     });
 
-    // IO
-    // io42 :: IO Int
-    var io42 = IO.return(42);
-    it('should bind to the right value', function(assert) {
-        IO.bind(io42, function(fortyTwo) {
-            assert(fortyTwo === 42);
-        });
-    });
-    // inc :: Int -> IO Int
-    var inc = compose(IO.return, function(n) { return n + 1; });
-    var io43 = IO.bind(io42, inc);
-    it('should get to correct result', function(assert) {
-        IO.bind(io43, function(n) {
-            assert(n === 43);
-        });
-    });
-    it('should return second function\'s result', function(assert) {
-        var io43Next = IO.next(io43, io43);
-        IO.bind(io43Next, function(n) {
-            assert(n === 43);
-        });
-    });
-    it('should result in correct value after executing', function(assert) {
-        assert(IO.main(io42) === 42);
-    });
-
-    // Delay & DelayList
-    // toutAdd42 :: Delay Int Int
-    var toutAdd42 = Delay.Timeout(0, compose(IO.return, function(n) { return n + 42; }));
-    // toutCheck :: _Assert -> a -> Delay a ()
-    var toutCheck = function(assert, expected) {
-        return Delay.Timeout(0, function(x) {
-            assert(x === expected);
-            return IO.return(null);
-        });
-    };
-    it('should add 42 after delaying', function(assert) {
-        var list = DelayList.Cons(toutAdd42, DelayList.Cons(toutCheck(assert, 42), DelayList.Empty()));
-        IO.main(DelayList.resolve(0, list));
-    });
-    it('should add 84 after delaying twice', function(assert) {
-        var list = DelayList.Cons(toutAdd42, DelayList.Cons(toutAdd42,
-            DelayList.Cons(toutCheck(assert, 84), DelayList.Empty())));
-        IO.main(DelayList.resolve(0, list));
-    });
-
-    // Delay & Listen
-    // eventPropIO :: String -> _Event -> a -> IO b
-    var eventPropIO = function(key) {
-        return function(ev, a) {
-            return compose(IO.return, prop(key))(constant(ev, a));
-        };
-    };
-    it('should result event\'s type after event triggered', function(assert) {
-        // lisType :: Delay a String
-        var lisType = Delay.Listened(Listen.DocEvent('someEvent'), eventPropIO('type'));
-        var list = DelayList.Cons(lisType, DelayList.Cons(toutCheck(assert, 'someEvent'), DelayList.Empty()));
-        IO.main(DelayList.resolve(null, list));
-        // unsafe
-        document.dispatchEvent(new Event('someEvent'));
-    });
-    it('should listen for an event after delayed', function(assert) {
-        // unsafe
-        // Dispatch two events, the first one should be missed.
-        setTimeout(function() {
-            document.dispatchEvent(new Event('someEvent'));
-        }, 5);
-        var timestamp;
-        setTimeout(function() {
-            var event = new Event('someEvent');
-            timestamp = event.timeStamp;
-            document.dispatchEvent(event);
-        }, 10);
-
-        // lisTime :: Delay a Float
-        var lisTime = Delay.Listened(Listen.DocEvent('someEvent'), eventPropIO('timeStamp'));
-        // toutWait :: Delay a ()
-        var toutWait = Delay.Timeout(7, function() {
-            return IO.return(null);
-        });
-        // toutCheckTimestamp :: Delay Float ()
-        // Can not use outside `toutCheck` because timestamp will be set after a while.
-        var toutCheckTimestamp = Delay.Timeout(0, function(x) {
-            assert(x === timestamp);
-            return IO.return(null);
-        });
-
-        var list = DelayList.Cons(toutWait, DelayList.Cons(lisTime,
-            DelayList.Cons(toutCheckTimestamp, DelayList.Empty())));
-        IO.main(DelayList.resolve(null, list));
-    });
-    // lisAdd42 :: Delay Int Int
-    var lisAdd42 = Delay.Listened(Listen.DocEvent('someEvent'), function(_, n) {
-        return IO.return(n + 42);
-    });
-    it('should pass the value after event triggered', function(assert) {
-        var list = DelayList.Cons(lisAdd42, DelayList.Cons(toutCheck(assert, 42), DelayList.Empty()));
-        IO.main(DelayList.resolve(0, list));
-        // unsafe
-        document.dispatchEvent(new Event('someEvent'));
-    });
-    it('should be able to listen one event for more than once', function(assert) {
-        var list = DelayList.Cons(lisAdd42, DelayList.Cons(lisAdd42,
-            DelayList.Cons(toutCheck(assert, 84), DelayList.Empty())));
-        IO.main(DelayList.resolve(0, list));
-        // unsafe
-        document.dispatchEvent(new Event('someEvent'));
-        document.dispatchEvent(new Event('someEvent'));
+    it('should be executed in seq', function(assert) {
+        assert(willSeq(function() {
+            return 42;
+        }, function(n) {
+            return function() {
+                return n + 1;
+            };
+        })() === 43);
     });
 
     test.end();
